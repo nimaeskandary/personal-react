190324384edb7d2170a2353473c1759e91757f68
nimaeskandary <nimaesk1@gmail.com>
2017-05-27 12:37:30 -0400

    add router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/App';
-import './custom-css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
