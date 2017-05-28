245c4b27603d83d261086a1d4f3a3b9374836b85
nimaeskandary <nimaesk1@gmail.com>
2017-05-28 17:11:09 -0400

    top navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
 import { IndexLinkContainer } from 'react-router-bootstrap';
 import FontAwesome from 'react-fontawesome';
 import '../custom-css/Sidebar.css';
 
-class Sidebar extends Component {
-  render() {
-    return (
-      <Nav className='sidebar-center' bsStyle="pills" stacked>
-        {
-          this.props.ROUTES.map((route, index) => (
-            <IndexLinkContainer to={route.path} key={index}>
-              <NavItem>
-                {route.title}&emsp;
-                <FontAwesome name={route.icon} />
-              </NavItem>
-            </IndexLinkContainer>
-          ))
-        }
-      </Nav>
-    );
-  };
+function Sidebar(props) {
+  return (
+    <Nav className='sidebar-center' bsStyle="pills" stacked>
+      {
+        props.ROUTES.map((route, index) => (
+          <IndexLinkContainer to={route.path} key={index}>
+            <NavItem>
+              <FontAwesome name={route.icon} />
+              &emsp; {route.title}
+            </NavItem>
+          </IndexLinkContainer>
+        ))
+      }
+    </Nav>
+  );
 }
 
 export default Sidebar
