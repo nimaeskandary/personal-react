ad2b54f03c9258891636474e3ffea6b28cb94216
nimaeskandary <nimaesk1@gmail.com>
2017-06-05 22:10:39 -0400

    build script plus resizing upgrades

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,54 +1,83 @@
-import React from 'react';
-import { Navbar, NavItem, Nav } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
-import '../custom-css/TopNavbar.css'
+import '../custom-css/TopNavbar.css';
 
-function viewLinks(mobile, ROUTES) {
-  if(mobile) {
+class TopNavbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+      colSmMin: 768
+    };
+  }
+
+  updateWidth() {
+      this.setState({width: window.innerWidth});
+  }
+
+  componentWillMount() {
+      this.updateWidth();
+  }
+
+  componentDidMount() {
+      window.addEventListener("resize", this.updateWidth.bind(this));
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener("resize", this.updateWidth.bind(this));
+  }
+
+  render() {
     return (
-      ROUTES.map((route) => (
-        <Navbar.Text key={route.title}>
-          <Link className='top-navbar-link' to={route.path}>
-            <FontAwesome name={route.icon} />&ensp;{route.title}
-          </Link>
-        </Navbar.Text>
-      ))
-    )
-  } else return ''
-}
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a target="_blank" rel="noopener noreferrer" href="https://cs.drexel.edu">
+              Drexel &ensp;
+              <FontAwesome name="university" />
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Text>
+            Nima Eskandary
+          </Navbar.Text>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <div className="pull-right">
+            {this.viewLinks(this.props.mobile, this.props.ROUTES)}
+            <Navbar.Text>
+              <Navbar.Link href="https://github.com/nimaeskandary/personal-react"
+                           target="_blank" rel="noopener noreferrer">
+                 <FontAwesome name="file-code-o" />&ensp;Github
+              </Navbar.Link>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Navbar.Link href="https://linkedin.com/in/nimaeskandary"
+                           target="_blank" rel="noopener noreferrer">
+                 <FontAwesome name="briefcase" />&ensp;LinkedIn
+              </Navbar.Link>
+            </Navbar.Text>
+        </div>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 
-function TopNavbar(props) {
-  return (
-    <Navbar inverse collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a target="_blank" rel="noopener noreferrer" href="https://cs.drexel.edu">
-            Drexel &ensp;
-            <FontAwesome name="university" />
-          </a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <div className="pull-right">
-          <Navbar.Text>
-            <Navbar.Link href="https://github.com/nimaeskandary/personal-react"
-                         target="_blank" rel="noopener noreferrer">
-               <FontAwesome name="file-code-o" />&ensp;Github
-            </Navbar.Link>
+  viewLinks(mobile, ROUTES) {
+    if(mobile || Number(this.state.width) < this.state.colSmMin) {
+      return (
+        ROUTES.map((route) => (
+          <Navbar.Text key={route.title}>
+            <Link className='top-navbar-link' to={route.path}>
+              <FontAwesome name={route.icon} />&ensp;{route.title}
+            </Link>
           </Navbar.Text>
-          <Navbar.Text>
-            <Navbar.Link href="https://linkedin.com/in/nimaeskandary"
-                         target="_blank" rel="noopener noreferrer">
-               <FontAwesome name="briefcase" />&ensp;LinkedIn
-            </Navbar.Link>
-          </Navbar.Text>
-          {viewLinks(props.mobile, props.ROUTES)}
-      </div>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+        ))
+      )
+    } else return ''
+  }
 };
 
 export default TopNavbar;
--- src/templates/Page.js
+++ src/templates/Page.js
@@ -3,10 +3,33 @@ import { Col, Panel } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 class Page extends Component {
+  constructor() {
+    super();
+    this.state = {
+      height: window.innerHeight
+    };
+  }
+
+  updateHeight() {
+    this.setState({height: window.innerHeight});
+  }
+
+  componentWillMount() {
+    this.updateHeight();
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateHeight.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateHeight.bind(this));
+  }
+
   render() {
     return (
       <Col sm={10}>
-        <Panel header={
+        <Panel style={{height: this.state.height}} header={
             <h3>
               <FontAwesome name={this.props.route.icon} />
               &emsp;{this.props.route.title}
